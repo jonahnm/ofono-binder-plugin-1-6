d9eac4a377e0909487561635262497a3264b3056
TailsTheNonNamedFox <jonahdb2@gmail.com>
2024-05-24 17:51:31 -0400

    ah silly me

--- src/binder_radio.c
+++ src/binder_radio.c
@@ -182,10 +182,10 @@ binder_radio_power_request_cb(
 {
     BinderRadioObject* self = THIS(user_data);
     const RADIO_INTERFACE iface = radio_client_interface(self->client);
-    const RADIO_RESP code = (iface >= RADIO_INTERFACE_1_5) ?
+    RADIO_RESP code = (iface >= RADIO_INTERFACE_1_5) ?
                             RADIO_RESP_SET_RADIO_POWER_1_5 :
                             RADIO_RESP_SET_RADIO_POWER;
-
+    if (iface == RADIO_INTERFACE_1_6) code = RADIO_REQ_SET_RADIO_POWER_1_6;
     GASSERT(self->pending_req == req);
     radio_request_unref(self->pending_req);
     self->pending_req = NULL;
@@ -216,7 +216,7 @@ binder_radio_submit_power_request(
      */
     GBinderWriter writer;
     const RADIO_INTERFACE iface = radio_client_interface(self->client);
-    const RADIO_REQ code = (iface == RADIO_INTERFACE_1_5) ?
+    RADIO_REQ code = (iface == RADIO_INTERFACE_1_5) ?
                            RADIO_REQ_SET_RADIO_POWER_1_5 :
                            RADIO_REQ_SET_RADIO_POWER;
     if (iface == RADIO_INTERFACE_1_6) code = RADIO_REQ_SET_RADIO_POWER_1_6;
